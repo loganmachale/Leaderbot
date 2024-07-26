84d4069f5f440a9c439d57d03b43d3f0b4e11f41
Logan <loganmachale@yahoo.ca>
2024-07-26 15:05:57 -0700

    dfsdf

--- leaderbot.py
+++ leaderbot.py
@@ -4,6 +4,12 @@ import discord
 # L_MAIN
 # L_BACKUP
 
+class Player:
+    def __init__(self, name, kd, wl)
+        self.name = name
+        self.kd = kd
+        self.wl = wl
+
 BOT_TOKEN = "MTI2NjI4NDAxNjExNDQ3MDk2Ng.GVS3M0.soc2PKfD2TvF9wq9V4S2EgJDYZo1ew4MGeyUgc"
 
 LEADERBOARD_CHANNEL = 1266283285516910705
@@ -11,10 +17,11 @@ COMMANDS_CHANNEL = 1266283316844429426
 RESULTS_CHANNEL = 1266283405050380302
 
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+# client = discord.Client(intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
-    print('hello world')
+    print('Bot online')
     global leaderboard_c
     global commands_c
     global results_c
@@ -22,26 +29,36 @@ async def on_ready():
     commands_c = bot.get_channel(COMMANDS_CHANNEL)
     results_c = bot.get_channel(RESULTS_CHANNEL)
 
-    await commands_c.send("hello world")
+    await commands_c.send("Bot connected")
 
 
-@bot.command()
+@bot.command
 async def hello(ctx):
     await ctx.send("ARRRRGH")
 
 
-# @client.event
-# async def on_message(message):
+@bot.event
+async def on_message(message):
+    author = message.author
+    content = message.content
+    in_channel = message.channel
+    if author != bot.user:
+        if in_channel == results_c:
+            await results_c.send('working')
+            await results_c.send(content)
+        else:
+            await in_channel.send('wrong channel')
 
 
-@bot.command()
+@bot.command
 async def refresh(ctx, lim):
     await commands_c.send('refreshing with last {} matches...'.format(lim))
     await get_results(ctx, lim)
-    
+
 
 def get_results(ctx, lim):
-    messages = ctx.results_c.history(limit=lim)
+    messages = results_c.history(limit=lim)
     return commands_c.send(messages)
+
 
 bot.run(BOT_TOKEN)
